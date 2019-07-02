7b13265030c352b948883aa41d24c4ef3bad8b07
le2m0vi <le2m0vi@gmail.com>
2019-07-02 22:21:43 +0800

    update

--- MyPlayground.playground/Pages/下标.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/下标.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var str = "Hello, playground"
 
-//: [Next](@next)
+struct TimesTable {
+    
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+    
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+
+print("six times three is \(threeTimesTable[6])")
+
+
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+print(numberOfLegs)
+
+
+/**
+ 
+ 下标选项
+ 下标可以接受任意数量的入参，并且这些入参可以是任意类型。下标的返回值也可以是任意类型。
+ 下标可以使用可变参数，并且可以提供默认参数数值，但是不能使用输入输出参数
+ 一个类或结构体可以根据自身需要提供多个下标实现，使用下标时将通过入参的数量和类型进行区分，自动匹配合适的下标，这就是下标的重载
+ 
+ */
--- MyPlayground.playground/Pages/继承.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/继承.xcplaygroundpage/Contents.swift
@@ -1,7 +1,113 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var str = "Hello, playground"
+/**
+ 
+ 在 Swift 中，继承是区分「类」与其它类型的一个基本特征
+ 
+ */
 
-//: [Next](@next)
+class Vehicle {
+    var currentSpeed = 0.0
+    var description: String {
+        return "traveling at \(currentSpeed) miles per hour"
+    }
+    func makeNoise() {
+        // 什么也不做——因为车辆不一定会有噪音
+        print("This is base method")
+    }
+}
+
+let somVehicle = Vehicle()
+
+print(somVehicle.description)
+
+/**
+ 
+ 定义子类：
+ class SomeClass: SomeSuperclass {
+ // 这里是子类的定义
+ }
+ 
+ */
+
+class Bicycle: Vehicle {
+    var hasBasket = false
+}
+
+let bicycle = Bicycle()
+bicycle.hasBasket = true
+
+bicycle.currentSpeed = 15.0
+
+print("Bicycle: \(bicycle.description)")
+
+
+
+class Tandem: Bicycle {
+    var currentNumberOfPassengers = 0
+}
+
+let tandem = Tandem()
+tandem.hasBasket = true
+tandem.currentNumberOfPassengers = 2
+tandem.currentSpeed = 22.0
+print("Tandem: \(tandem.description)")
+
+/**
+ 
+ 重写：
+ 子类可以为继承来的实例方法，类方法，实例属性，类属性，或下标提供自己定制的实现。我们把这种行为叫重写
+ 
+ 重写某个特性，你需要在重写定义的前面加上 override 关键字
+ 
+ */
+
+class SomeCar: Vehicle {
+    
+    override func makeNoise() {
+        
+        super.makeNoise()
+        print("This is son method")
+    }
+    
+}
+
+let benzi = SomeCar()
+print(benzi.makeNoise())
+
+
+//重写属性---重写属性的 Getters 和 Setters
+
+class Car: Vehicle {
+    var gear = 1
+    override var description: String {
+        return super.description + " in gear \(gear)"
+    }
+}
+
+let car = Car()
+car.currentSpeed = 25.0
+car.gear = 3
+print("Car: \(car.description)")
+// 打印“Car: traveling at 25.0 miles per hour in gear 3”
+
+// 重写属性观察器
+class AutomaticCar: Car {
+    override var currentSpeed: Double {
+        didSet {
+            gear = Int(currentSpeed / 10.0) + 1
+        }
+    }
+}
+
+let automatic = AutomaticCar()
+automatic.currentSpeed = 35.0
+print("AutomaticCar: \(automatic.description)")
+// 打印“AutomaticCar: traveling at 35.0 miles per hour in gear 4”
+
+/**
+ 防止重写
+把方法，属性或下标标记为 final 来防止它们被重写
+ 只需要在声明关键字前加上 final 修饰符即可（例如：final var、final func、final class func 以及 final subscript）
+*/
