4352a69a0e04b8cd449b4826ad4f882cdd3fd36d
le2m0vi <le2m0vi@gmail.com>
2019-07-03 07:44:49 +0800

    update

--- MyPlayground.playground/Pages/构造析构.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/构造析构.xcplaygroundpage/Contents.swift
@@ -1,4 +1,77 @@
 
 import Foundation
 
+/**
+ 
+ 构造
+ 与 Objective-C 中的构造器不同，Swift 的构造器没有返回值。它们的主要任务是保证某种类型的新实例在第一次使用前完成正确的初始化
+ 
+ */
 
+/**
+ 存储属性的初始赋值
+ 类和结构体在创建实例时，必须为所有存储型属性设置合适的初始值。存储型属性的值不能处于一个未知的状态。
+ 
+ 你可以在构造器中为存储型属性设置初始值，也可以在定义属性时分配默认值
+ 
+ 注意
+ 当你为存储型属性分配默认值或者在构造器中为设置初始值时，它们的值是被直接设置的，不会触发任何属性观察者。
+ 
+ */
+
+struct Fahrenheit {
+    var temperature: Double
+    init() {
+        temperature = 32.0
+    }
+}
+var f = Fahrenheit()
+print("The default temperature is \(f.temperature)° Fahrenheit")
+// 打印“The default temperature is 32.0° Fahrenheit”
+
+/**
+ 自定义构造过程
+ */
+
+struct Celsius {
+    var temperatureInCelsius: Double
+    init(fromFahrenheit fahrenheit: Double) {
+        temperatureInCelsius = (fahrenheit - 32.0) / 1.8
+    }
+    init(fromKelvin kelvin: Double) {
+        temperatureInCelsius = kelvin - 273.15
+    }
+    
+    init(_ celsius: Double){
+        temperatureInCelsius = celsius
+    }
+}
+
+let boilingPointOfWater = Celsius(fromFahrenheit: 212.0)
+// boilingPointOfWater.temperatureInCelsius 是 100.0
+let freezingPointOfWater = Celsius(fromKelvin: 273.15)
+// freezingPointOfWater.temperatureInCelsius 是 0.0
+
+struct Color {
+    let red, green, blue: Double
+    init(red: Double, green: Double, blue: Double) {
+        self.red   = red
+        self.green = green
+        self.blue  = blue
+    }
+    init(white: Double) {
+        red   = white
+        green = white
+        blue  = white
+    }
+}
+let veryGreen = Color(red: 0.0, green: 1.0, blue: 0.0)
+
+let bodyTemperature = Celsius(37)
+
+/**
+ 
+ 可选属性类型
+ 如果你自定义的类型有一个逻辑上允许值为空的存储型属性——无论是因为它无法在初始化时赋值，还是因为它在之后某个时机可以赋值为空——都需要将它声明为 可选类型。可选类型的属性将自动初始化为 nil，表示这个属性是特意在构造过程设置为空
+ 
+ */
