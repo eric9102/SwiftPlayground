0f53f8a50991ed458267c8448127bbecc85c8b66
eric <rosylxf@gmail.com>
2019-07-02 13:46:30 +0800

    update

--- MyPlayground.playground/Pages/方法.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/方法.xcplaygroundpage/Contents.swift
@@ -2,6 +2,85 @@
 
 import Foundation
 
-var str = "Hello, playground"
+
+struct Point {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+var somePoint = Point(x: 1.0, y: 1.0)
+somePoint.moveBy(x: 2.0, y: 3.0)
+print("The point is now at (\(somePoint.x), \(somePoint.y))")
+// 打印“The point is now at (3.0, 4.0)”
+
+
+
+//注意，不能在结构体类型的常量（a constant of structure type）上调用可变方法，因为其属性不能被改变，即使属性是变量属性
+
+//let fixedPoint = Point(x: 3.0, y: 3.0)
+//fixedPoint.moveBy(x: 2.0, y: 3.0)
+// 这里将会报告一个错误
+
+
+/**
+ 
+ 在可变方法中给 self 赋值
+ 可变方法能够赋给隐含属性 self 一个全新的实例
+ 
+ */
+
+struct Point1 {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = Point1(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+// 枚举的可变方法可以把 self 设置为同一枚举类型中不同的成员：
+
+enum TriStateSwitch {
+    case off, low, high
+    mutating func next() {
+        switch self {
+        case .off:
+            self = .low
+        case .low:
+            self = .high
+        case .high:
+            self = .off
+        }
+    }
+}
+var ovenLight = TriStateSwitch.low
+ovenLight.next()
+// ovenLight 现在等于 .high
+ovenLight.next()
+// ovenLight 现在等于 .off
+
+
+/**
+ 
+ 类型方法
+ 定义在类型本身上调用的方法，这种方法就叫做类型方法。在方法的 func 关键字之前加上关键字 static，来指定类型方法。
+ 类还可以用关键字 class 来指定，从而允许子类重写父类该方法的实现
+ 
+ */
+
+class SomeClass {
+    class func someTypeMethod() {
+        // 在这里实现类型方法
+    }
+}
+SomeClass.someTypeMethod()
+
+
+
+
+
+
+
+
 
 //: [Next](@next)
