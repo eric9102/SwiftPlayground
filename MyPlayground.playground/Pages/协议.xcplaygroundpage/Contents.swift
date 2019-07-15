c0fd5ac19d07be1f806e00df957d9258832a0a09
le2m0vi <le2m0vi@gmail.com>
2019-07-16 00:07:48 +0800

    update

--- MyPlayground.playground/Pages/协议.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/协议.xcplaygroundpage/Contents.swift
@@ -1,1 +1,165 @@
+import UIKit
+
+/**
+ 
+ 协议 定义了一个蓝图，规定了用来实现某一特定任务或者功能的方法、属性，以及其他需要的东西
+ 类、结构体或枚举都可以遵循协议，并为协议定义的这些要求提供具体实现
+ 
+ protocol SomeProtocol {
+ // 这里是协议的定义部分
+ }
+ 
+ //遵循多个协议
+ struct SomeStructure: FirstProtocol, AnotherProtocol {
+ // 这里是结构体的定义部分
+ }
+ 
+ // 一个拥有父类的类在遵循协议时，赢该将父类名放在协议名之前，以逗号分隔
+ class SomeClass: SomeSuperClass, FirstProtocol, AnotherProtocol {
+ // 这里是类的定义部分
+ }
+ 
+ */
+
+//协议总是用 var 关键字来声明变量属性，在类型声明后加上 { set get } 来表示属性是可读可写的，可读属性则用 { get } 来表示：
+
+protocol SomeProtocol {
+    var mustBeSettable: Int { get set }
+    var doesNotNeedToBeSettable: Int { get }
+}
+
+//在协议中定义类型属性时，总是使用 static 关键字作为前缀。当类类型遵循协议时，除了 static 关键字，还可以使用 class 关键字来声明类型属性：
+
+protocol AnotherProtocol {
+    static var someTypeProperty: Int { get set }
+}
+
+//
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+let john = Person(fullName: "John Appleseed")
+// john.fullName 为 "John Appleseed"
+
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+// ncc1701.fullName 为 "USS Enterprise"
+
+//方法要求
+/**
+ 协议可以要求遵循协议的类型实现某些指定的实例方法或类方法。这些方法作为协议的一部分，像普通方法一样放在协议的定义中，但是不需要大括号和方法体。可以在协议中定义具有可变参数的方法，和普通方法的定义方式相同。但是，不支持为协议中的方法提供默认参数。
+ 正如属性要求中所述，在协议中定义类方法的时候，总是使用 static 关键字作为前缀。即使在类实现时，类方法要求使用 class 或 static 作为关键字前缀，前面的规则仍然适用：
+ */
+
+protocol SSomeProtocol {
+    static func someTypeMethod()
+}
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+//
+
+protocol Togglable {
+    mutating func toggle()
+}
+
+enum OnOffSwitch: Togglable {
+    case off, on
+    mutating func toggle() {
+        switch self {
+        case .off:
+            self = .on
+        case .on:
+            self = .off
+        }
+    }
+}
+var lightSwitch = OnOffSwitch.off
+lightSwitch.toggle()
+// lightSwitch 现在的值为 .on
+
+// 构造器要求
+//协议可以要求遵循协议的类型实现指定的构造器。你可以像编写普通构造器那样，在协议的定义里写下构造器的声明，但不需要写花括号和构造器的实体：
+
+/**
+ 协议构造器要求的类实现
+ 你可以在遵循协议的类中实现构造器，无论是作为指定构造器，还是作为便利构造器。无论哪种情况，你都必须为构造器实现标上 required 修饰符：
+ 
+ 使用 required 修饰符可以确保所有子类也必须提供此构造器实现，从而也能符合协议。
+ 
+ */
+
+protocol ASomeProtocol {
+    init(someParameter: Int)
+}
+
+class ASomeClass: ASomeProtocol {
+    required init(someParameter: Int) {
+        // 这里是构造器的实现部分
+    }
+}
+
+/**
+ 如果一个子类重写了父类的指定构造器，并且该构造器满足了某个协议的要求，
+ 那么该构造器的实现需要同时标注 required 和 override 修饰符：
+ */
+
+protocol BSomeProtocol {
+    init()
+}
+
+class BSomeSuperClass {
+    init() {
+        // 这里是构造器的实现部分
+    }
+}
+
+class BSomeSubClass: BSomeSuperClass, BSomeProtocol {
+    // 因为遵循协议，需要加上 required
+    // 因为继承自父类，需要加上 override
+    required override init() {
+        // 这里是构造器的实现部分
+    }
+}
+
+/**
+ 协议作为类型
+ 尽管协议本身并未实现任何功能，但是协议可以被当做一个功能完备的类型来使用。协议作为类型使用，有时被称作「存在类型」，
+ 这个名词来自「存在着一个类型 T，该类型遵循协议 T」。
+ 
+ 协议可以像其他普通类型一样使用，使用场景如下：
+ 作为函数、方法或构造器中的参数类型或返回值类型
+ 作为常量、变量或属性的类型
+ 作为数组、字典或其他容器中的元素类型
+ */
+//下面是将协议作为类型使用的例子：
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
 
