c63a1bc80b2481a76bb775a72882affe320ae96b
le2m0vi <le2m0vi@gmail.com>
2019-07-15 22:50:24 +0800

    update

--- MyPlayground.playground/Pages/扩展.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/扩展.xcplaygroundpage/Contents.swift
@@ -1,7 +1,141 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var str = "Hello, playground"
+/**
+ Swift 中的扩展可以：
+ 添加计算型实例属性和计算型类属性
+ 定义实例方法和类方法
+ 提供新的构造器
+ 定义下标
+ 定义和使用新的嵌套类型
+ 使已经存在的类型遵循（conform）一个协议
+ */
 
-//: [Next](@next)
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+let oneInch = 25.4.mm
+print("One inch is \(oneInch) meters")
+// 打印“One inch is 0.0254 meters”
+let threeFeet = 3.ft
+print("Three feet is \(threeFeet) meters")
+// 打印“Three feet is 0.914399970739201 meters”
+
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0),
+                          size: Size(width: 5.0, height: 5.0))
+
+memberwiseRect.origin
+
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+
+/**
+ 扩展可以给现有类型添加新的实例方法和类方法
+ */
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+
+3.repetitions {
+    print("hello!")
+}
+
+/**
+ 通过扩展添加的实例方法同样也可以修改（或 mutating（改变））实例本身。结构体和枚举的方法，若是可以修改 self 或者它自己的属性，
+ 则必须将这个实例方法标记为 mutating，就像是改变了方法的原始实现。
+ */
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square()
+
+/**
+ 下标
+ */
+
+
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+        for _ in 0..<digitIndex {
+            decimalBase *= 10
+        }
+        return (self / decimalBase) % 10
+    }
+}
+746381295[0]
+// 返回 5
+746381295[1]
+// 返回 9
+746381295[2]
+// 返回 2
+746381295[8]
+// 返回 7
+
+/**
+ 扩展可以给现有的类，结构体，还有枚举添加新的嵌套类型
+ */
+
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
+
+func printIntegerKinds(_ numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case .negative:
+            print("- ", terminator: "")
+        case .zero:
+            print("0 ", terminator: "")
+        case .positive:
+            print("+ ", terminator: "")
+        }
+    }
+    print("")
+}
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
+// 打印“+ + - 0 - 0 + ”
