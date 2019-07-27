6486c16f544d53724863d81a72a57c3d09995d0f
le2m0vi <le2m0vi@gmail.com>
2019-07-28 06:12:43 +0800

    update

--- SwiftTips.playground/Pages/Reflection和Mirror.xcplaygroundpage/Contents.swift
+++ SwiftTips.playground/Pages/Reflection和Mirror.xcplaygroundpage/Contents.swift
@@ -2,6 +2,19 @@
 
 import Foundation
 
-var str = "Hello, playground"
+struct Person {
+    let name: String
+    let age: Int
+}
+
+let xiaoMing = Person(name: "xiiaoming", age: 16)
+let r = Mirror(reflecting: xiaoMing)
+print("xiaoMing iS \(r.displayStyle)")
+
+print("属性个数: \(r.children.count)")
+for child in r.children {
+    print("属性名：\(String(describing:child.label)), 值：\(child.value)")
+}
+
 
 //: [Next](@next)
