f42ab1baf7b65894176bec822fa829563b5d0eb9
eric <rosylxf@gmail.com>
2019-07-29 15:00:52 +0800

    update

--- SwiftTips.playground/Pages/Selector.xcplaygroundpage/Contents.swift
+++ SwiftTips.playground/Pages/Selector.xcplaygroundpage/Contents.swift
@@ -2,6 +2,23 @@
 
 import Foundation
 
+class A {
+    
+    @objc func callMe() {
+        
+        print("callMe is called")
+    }
+    
+    @objc func callOne() {
+        
+        let method1 = #selector(callMe)
+        print(method1)
+    }
+}
+
+let objA = A()
+objA.callOne()
+
 
 
 //: [Next](@next)
